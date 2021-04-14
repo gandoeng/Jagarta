7887081164babe1bfab71bb9a6857639b2043e90
gandoeng <nursanogandung@yahoo.co.id>
2021-04-14 15:24:22 +0700

    section value

--- Jagarta_web/resources/views/frontend/home.blade.php
+++ Jagarta_web/resources/views/frontend/home.blade.php
@@ -72,11 +72,11 @@
 									</div>
 
 								<a class="carousel-control-prev" href="#carouselAbout" role="button" data-slide="prev">
-									<i class="fas fa-angle-double-left" style="font-size: 50px; color:  #AE5528"></i>
+									<i class="fas fa-angle-double-left carousel-but"></i>
    									<span class="sr-only">Previous</span>
 								</a>
 								<a class="carousel-control-next" href="#carouselAbout" role="button" data-slide="next">
-    								<i class="fas fa-angle-double-right" style="font-size: 50px; color:  #AE5528"></i>    							
+    								<i class="fas fa-angle-double-right carousel-but" ></i>
     								<span class="sr-only">Next</span>
  								</a>
 								</div>
@@ -87,8 +87,19 @@
 			</div>
 		</section>
 
-		<section style="">
-			
+		<section style="width: 100%; height: 400px; background-image: url({{asset('img/value.png')}}); background-size: cover; border-top: none; padding-top: 1%;">
+			<div class="col-lg-12" style="height: 200px;">
+				<div class="container" style="height: 300px;">
+					<div class="row" style="height: 300px;">
+						<div class="col-lg-12">
+							<h1 class="text-center value-title">Our Value <hr class="about-border"> </h1>
+						</div>
+						<div class="col-lg-12" style="position: relative; left: 0; right: 0; margin-left: 15%;" >
+							<p style="text-align: center; font-family: 'jagarta'; color: white; width: 800px;">Irure dolore incididunt veniam eiusmod tempor tempor dolore enim voluptate aute aliqua exercitation veniam magna dolor est dolor do ea nisi occaecat irure deserunt veniam ut mollit exercitation do eiusmod dolore duis qui ut sint amet do nisi cupidatat laborum cillum aliquip tempor dolore quis eiusmod do veniam minim commodo laborum aliqua mollit in adipisicing commodo adipisicing proident non laborum est commodo qui pariatur deserunt sint cillum occaecat ut labore dolore ut dolore excepteur ea in ex labore magna laboris occaecat in ea nisi elit ut irure in non elit excepteur consectetur commodo ut dolor fugiat ut aliquip duis ut ex occaecat cupidatat reprehenderit dolor adipisicing nostrud sed quis quis consectetur dolore in in ut nostrud sunt .</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	</main>
 @endsection
